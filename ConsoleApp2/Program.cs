4d40d636d8ee232955e2ca560ad80c25e22f8d92
ViktorVorobyev06 <43294723+ViktorVorobyev06@users.noreply.github.com>
2018-09-18 14:05:32 +0600

    Первое задание я закоментировал

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Viktor Vorobyev");
+
+            Console.Write("Введите а: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите в: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("Результат с = {0}", c);
+           
+        }
+    }
+}
